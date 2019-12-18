269bc6c807d92df6be3423c2b52d890f7e9c5a40
Matteo <matteop18@gmail.com>
2019-12-18 15:04:03 +0100

    primo commit

---
+++ js/biglietto_treno.js
@@ -0,0 +1,22 @@
+// Calcolo prezzo del treno:
+// Il programma dovrà chiedere all’utente il
+// numero di chilometri che vuole
+// percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà
+// calcolare il prezzo totale del viaggio.
+// Il prezzo del biglietto è definito in base ai
+// km (0.21 € al km), ma va applicato uno
+// sconto del 20% per i minorenni e del
+// 40% per gli over 65.
+
+// Chiedere i km che vuole percorrere il passeggero
+
+// Chiedere l'età del passeggero
+
+// dichiarare una variabile costoBiglietto che 1 km equivale a 0,21 euro
+
+// moltiplicare i km inseriti del passeggero con la variabile costoBiglietto
+
+// inserire la condizione che se il passeggero è minorenne verrà applicato un 20% di sconto
+
+// inserire la condizione che il passeggero over 65 pagherà con un 40% di sconto
